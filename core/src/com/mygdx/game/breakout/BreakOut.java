33304a675845c6b7692254c49a5ffae576ef033f
CristoDev <christophe.lefevre.1977@gmail.com>
2019-09-20 16:17:38 +0200

    changement de l'atlas

--- core/src/com/mygdx/game/breakout/BreakOut.java
+++ core/src/com/mygdx/game/breakout/BreakOut.java
@@ -1,4 +1,21 @@
 package com.mygdx.game.breakout;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
 public class BreakOut {
+    private static final String ATLAS="breakout/breakout.atlas";
+
+    private Image paddle;
+
+    public BreakOut() {
+
+    }
+
+    private void init() {
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(ATLAS));
+        paddle=new Image(atlas.findRegion("paddle_11"));
+        paddle.setPosition(100, 300);
+    }
 }

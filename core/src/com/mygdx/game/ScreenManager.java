0f94d30845b88b53abee302fd5d3aa1a39804126
CristoDev <christophe.lefevre.1977@gmail.com>
2019-09-20 15:25:34 +0200

    Suppression des premiers fichiers pour BreakOut

--- core/src/com/mygdx/game/ScreenManager.java
+++ core/src/com/mygdx/game/ScreenManager.java
@@ -3,25 +3,19 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.mygdx.game.screens.*;
-import com.mygdx.game.tetris.Grid;
-import com.mygdx.game.tetris.GridScreen;
 
 public class ScreenManager extends Game {
     private static MenuScreen _menuScreen;
     private static WindowScreen _windowScreen;
     private static ImageScreen _imageScreen;
     private static ImageMenuScreen _imageMenuScreen;
-    private static ScrollingScreen _scrollingScreen;
     private static BarScreen _barScreen;
-    private static GridScreen _gridScreen;
 
     public static enum ScreenType{
         MenuScreen,
         ImageScreen,
         ImageMenuScreen,
-        ScrollingScreen,
         BarScreen,
-        GridScreen,
         WindowScreen;
     }
 
@@ -35,12 +29,8 @@ public class ScreenManager extends Game {
                 return _imageScreen;
             case ImageMenuScreen:
                 return _imageMenuScreen;
-            case ScrollingScreen:
-                return _scrollingScreen;
             case BarScreen:
                 return _barScreen;
-            case GridScreen:
-                return _gridScreen;
             default:
                 return _menuScreen;
         }
@@ -53,12 +43,9 @@ public class ScreenManager extends Game {
         //_windowScreen=new WindowScreen(this);
         //_imageScreen=new ImageScreen(this);
         //_imageMenuScreen=new ImageMenuScreen(this);
-        //_scrollingScreen=new ScrollingScreen(this);
         //_barScreen=new BarScreen(this);
-        _gridScreen=new GridScreen(this);
         //setScreen(_menuScreen);
-        setScreen(_gridScreen);
-        debug();
+        setScreen(_menuScreen);
     }
 
     @Override
@@ -67,13 +54,6 @@ public class ScreenManager extends Game {
         _windowScreen.dispose();
         _imageScreen.dispose();
         _imageMenuScreen.dispose();
-        _scrollingScreen.dispose();
         _barScreen.dispose();
-        _gridScreen.dispose();
-    }
-
-    private void debug() {
-        Grid g=new Grid();
-        g.todo();
     }
 }
